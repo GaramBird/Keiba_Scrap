5d9284b8803cbd4ceb4e5249f4983630169488ab
GARAMBird <tyuityui1023@gmail.com>
2019-01-29 02:12:57 +0900

    test

--- Keiba_Scrap/My Project/Application.Designer.vb
+++ Keiba_Scrap/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Keiba_Scrap.Form1
+            Me.MainForm = Global.Keiba_Scrap.ShowFram
         End Sub
     End Class
 End Namespace
