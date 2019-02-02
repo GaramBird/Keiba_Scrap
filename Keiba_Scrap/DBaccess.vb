cda47b9a260c42edc34d43df43cffa11907f6052
GARAMBird <tyuityui1023@gmail.com>
2019-02-02 14:57:39 +0900

    a

---
+++ Keiba_Scrap/DBaccess.vb
@@ -0,0 +1,37 @@
+﻿Imports Oracle.DataAccess.Client
+Imports Oracle.DataAccess.Types
+
+
+Public Class DBaccess
+    Private Function GetaccessString() As Boolean
+        Dim strUser As String 'ユーザー
+        Dim strPasswd As String 'パスワード
+        Dim strDataSource As String 'データソース
+        Dim strConnectstring As String
+        strUser = "birdDB"
+        strPasswd = "tori1023"
+        strDataSource = "orcl"
+        strConnectstring = "User Id=" & strUser & "; " & "Password=" & strPasswd & "; " & "Data Source=" & strDataSource
+    End Function
+
+    Private Function DbOpen(ByVal constr As String) As Boolean
+        Dim oraCon As OracleConnection
+        oraCon = New OracleConnection(constr)
+
+        Try
+            oraCon.Open()
+            Return True
+        Catch ex As Exception
+            MessageBox.Show("データベースアクセスエラー")
+            Return False
+        End Try
+    End Function
+
+    Private Sub DbTransaction()
+        Dim oraTran As OracleTransaction
+        oraTran.Commit()
+        oraTran.Rollback()
+
+    End Sub
+
+End Class
--- Keiba_Scrap/Scrap.vb
+++ Keiba_Scrap/Scrap.vb
@@ -1,6 +1,58 @@
 ﻿'競馬スクレイピングクラス
 
 Public Class Scrap
+    Private Sub Scraping()
+        'URL情報の操作オブジェクト
+        Dim sURL As String
+        Dim sHTML As String
+        Dim objWC As System.Net.WebClient
+        Dim objDOC As HtmlAgilityPack.HtmlDocument
+        Dim objNodes As HtmlAgilityPack.HtmlNodeCollection
+        Dim objH_name As HtmlAgilityPack.HtmlNodeCollection
+        Dim objRace As HtmlAgilityPack.HtmlNodeCollection
+
+        '取得したリスト
+        Dim race As New List(Of String)
+        Dim h_name As New List(Of String)
+        Dim itisoumae As New List(Of String)
+        Dim nisoumae As New List(Of String)
+        Dim sansoumae As New List(Of String)
+        Dim yonsoumae As New List(Of String)
+        Dim tosoumae As New List(Of String)
+
+
+        '変数宣言
+        sURL = "https://race.netkeiba.com/?pid=race&id=c201806050811&mode=shutuba"
+        objWC = New System.Net.WebClient()
+        objWC.Encoding = System.Text.Encoding.GetEncoding("euc-jp")
+        sHTML = objWC.DownloadString(sURL)
+
+        objDOC = New HtmlAgilityPack.HtmlDocument()
+        objDOC.LoadHtml(sHTML)
+
+        objNodes = objDOC.DocumentNode.SelectNodes("//table[@class=""race_table_01 nk_tb_common shutuba_table""]/tr/td")
+        objH_name = objDOC.DocumentNode.SelectNodes("//table[@class=""race_table_01 nk_tb_common shutuba_table""]/tr/td[@class=""txt_l""]/span[@class=""h_name""]/a")
+        objRace = objDOC.DocumentNode.SelectNodes("//table[@class=""race_table_01 nk_tb_common shutuba_table""]/tr/td[@class=""txt_l""]/span[class=""race_name""]")
+
+        '取得するデータテーブルを定義する。
+
+        Dim dt As DataTable = New DataTable
+        dt.Columns.Add("馬名")
+
+
+        For Each row In objDOC.DocumentNode.SelectNodes("//table[@class=""race_table_01 nk_tb_common shutuba_table""]/tr")
+            Dim Nodes = row.SelectNodes("td/span[@class=""h_name""]")
+            Dim h_namea = Nodes
+            dt.Rows.Add(h_name)
+        Next
+
+
+        For Each row In objDOC.DocumentNode.SelectNodes("//table[@class=""race_table_01 nk_tb_common shutuba_table""]/tr[2]")
+            Dim Nodes = row.SelectNodes("td/span")
+        Next
+        dt.Dispose()
+
+    End Sub
 
 
 End Class
